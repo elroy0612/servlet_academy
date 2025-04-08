230f80b7c77c650594618df0d241aa2c00998ef2
test <test@test.com>
2025-04-08 21:35:08 +0900

    노트북에서 코드 추가

--- src/main/java/com/test/lesson01/GetMethodQuiz07.java
+++ src/main/java/com/test/lesson01/GetMethodQuiz07.java
@@ -14,6 +14,8 @@ public class GetMethodQuiz07 extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
+		// 노트북에서 주석 추가
+		
 		response.setContentType("text/html");
 		response.setCharacterEncoding("utf-8");
 		
